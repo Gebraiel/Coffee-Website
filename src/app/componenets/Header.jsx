932dd22ea727bb7bb941d91ccc067abc1948e0d5
Gebraiel <67439330+Gebraiel@users.noreply.github.com>
2025-04-25 23:51:24 +0300

    .

--- src/app/componenets/Header.jsx
+++ src/app/componenets/Header.jsx
@@ -6,9 +6,9 @@ import mobileHeaderBG from "@/public/bg-cafe-sm.jpg";
 export default function Header(){
     return(
         <header className="relative w-full lg:h-[300px]">
-            <Image className="object-cover lg:block hidden" fill src={desktopHeaderBG} />
-            <Image className="hidden sm:block lg:hidden"src={tabletHeaderBG} />
-            <Image className="block sm:hidden"src={mobileHeaderBG} />
+            <Image className="object-cover lg:block hidden" alt="Header Image" fill src={desktopHeaderBG} />
+            <Image className="hidden sm:block lg:hidden" alt="Header Image" src={tabletHeaderBG} />
+            <Image className="block sm:hidden" alt="Header Image" src={mobileHeaderBG} />
         </header>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default async function Home() {
   return (
       <div className="bg-[rgb(27,29,31)] w-[90%] max-w-8xl sm:p-14 p-5 absolute rounded-2xl xl:top-1/6 lg:top-1/4 top-1/8">
         <MotionComponent as={'div'} props={{initial:{opacity:0},animate:{opacity:1}}} className="text-center">
-            <h1 className="sm:text-[2rem] text-2xl font-bold text-center">Our Collection</h1> 
-            <p className="w-[90%] max-w-[500px] text-[#6F757C] sm:text-base text-xs mt-3 m-auto">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
+            <h1 className="sm:text-[2rem] text-white text-2xl font-bold text-center">Our Collection</h1> 
+            <p className="w-[90%] max-w-[500px] text-[#6F757C] sm:text-base text-xs mt-3 m-auto text-center">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
         </MotionComponent>
         <CoffeeList products={products}/>
       </div>
